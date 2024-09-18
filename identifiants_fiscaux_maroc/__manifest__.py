18ff8dd6f5b3f906c1761957a08b1c314affd16c
mohamed lahlaili <100442993+lahlaili@users.noreply.github.com>
2024-09-18 18:54:52 +0100

    Add files via upload

--- identifiants_fiscaux_maroc/__manifest__.py
+++ identifiants_fiscaux_maroc/__manifest__.py
@@ -1,25 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Identifiants fiscaux marocains",
+    'name': "Identifiants fiscaux maroc",
 
     'summary': """
     
-        identifiants fiscaux marocains
+        Ce module Odoo permet d'ajouter six nouveaux champs importants dans la fiche client : 
+        1. RC (Registre de Commerce) 
+        2. TP : Taxe profesionnelle 
+        3. IF (Identifiant Fiscale) 
+        4. CNSS : Numéro d'immaticulation CNSS
+        5. ICE (Identifiant Commun de l'Entreprise)
+        6. CAPITAL de la société
+
+        Ces champs facilitent la gestion et le suivi des informations légales et fiscales des entreprises clientes dans le système Odoo. Ce module est essentiel pour les entreprises nécessitant un suivi précis de leurs clients dans le respect des obligations légales.
+
     
     """,
 
     'description': """
-       
+       Ajout des champs RC, TP, IF, CNSS, ICE et CAPITAL dans la fiche société ainsi que les fiches clients & fournisseurs .
+
     """,
 
-    'author': 'DBC',
-    'company': 'DBC',
-    'maintainer': 'DBC',
+    'author': 'D-Business Consulting',
+    'company': 'D-Business Consulting',
+    'maintainer': 'D-Business Consulting',
     'website': 'https://d-business-consulting.ma',
-    'category': 'purchase',
+    'category': 'Sales,Purchases',
     'version': '16.0.1.0.0',
 
-    'depends': ['base','account'],
+    'depends': ['base'],
 
     'data': [
         'views/view.xml'
